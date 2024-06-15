9a8af46e42bbd33727ebcfb2660ec8d4182ea5e2
Yuvraj210103 <ys146228@gmail.com>
2024-06-15 11:28:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [position, setPosition] = useState({ top: 50, left: 50 });
+  const [direction, setDirection] = useState("right");
+  const prevPosition = useRef(position);
+
+  const handleClick = (e) => {
+    const { clientX, clientY } = e;
+    const logoWidth = 100;
+    const logoHeight = 100;
+
+    const newPosition = {
+      top: clientY - logoHeight / 2,
+      left: clientX - logoWidth / 2,
+    };
+
+    const isMovingRight = newPosition.left > prevPosition.current.left;
+    setDirection(isMovingRight ? "right" : "left");
+
+    setPosition(newPosition);
+    prevPosition.current = newPosition;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onClick={handleClick}>
+      <img
+        src="https://www.wizard.financial/static/media/wizaart-img.56787174.gif"
+        alt="Logo"
+        className={`logo ${direction}`}
+        style={{ top: `${position.top}px`, left: `${position.left}px` }}
+      />
     </div>
   );
 }
